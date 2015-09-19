632c678b3f8278383e68832d613505216ee541fa
Angel Angelov <hextwoa@gmail.com>
2015-09-19 12:09:28 +0300

    threads!

--- info.py
+++ info.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 import i3ipc
+import threading
+from queue import Queue
+
+class PanelItem:
+    def __init__(self, key):
+        self.key = key
+        self.panel_data = ''
+
+    def text(self, text):
+        self.panel_data += text
+        return self
+
 class WindowManager:
     def __init__(self):
         self._i3 = None
@@ -13,13 +25,35 @@ class WindowManager:
 
     def loop(self, events):
         def workspace_event(connection, event):
-            self.refresh()
+            self.refresh_workspaces(events)
         self.i3.on('workspace::focus', workspace_event)
 
         self.i3.main()
 
-    def refresh(self):
-        print(self.i3.get_workspaces())
+    def refresh_workspaces(self, events):
+        info = PanelItem('workspaces')
+        for workspace in self.i3.get_workspaces():
+            info.text(workspace.name + ' ')
+        events.put(info)
 
 
-WindowInfo().loop(None)
+class EventLoop:
+    def __init__(self):
+        self.window_manager = WindowManager()
+        self.events = Queue()
+
+    def start_thread(self, loop, args):
+        thread = threading.Thread(target=loop, args=args)
+        thread.daemon = True
+        thread.start()
+        return thread
+
+    def loop(self):
+        self.window_manager_thread = self.start_thread(
+            self.window_manager.loop, (self.events,)
+        )
+
+        while True:
+            print(self.events.get().panel_data)
+
+EventLoop().loop()
