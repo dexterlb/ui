e2fd5042bc394711a7d68a908718378ae2fada88
dexterlb <dexterlb@qtrp.org>
2018-07-18 15:07:16 +0300

    move one dir up

---
+++ serve.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+server='naboo.qtrp.org'
+remote_path='/srv/http/rnd'
+webroot='http://rnd.qtrp.org'
+
+if [[ -n "${2}" ]]; then
+    filename="${2}"
+else
+    filename="$(basename "${1}")"
+fi
+
+ssh "${server}" rm -rvf "${remote_path}"/"${filename}" || exit 1
+scp -r "${1}" "${server}:${remote_path}"/"${filename}" || exit 1
+
+address="${webroot}/${filename}"
+
+echo -n "${address}" | xclip -sel clip
+echo "${address} copied"
---
+++ xorg/startx.sh
@@ -0,0 +1,63 @@
+# load from your bash or zsh profile
+
+
+function start_x {
+    # get the current TTY. We'll tell X to run on the same TTY to avoid problems.
+    TTY=$(tty)
+    TTY=${TTY#/dev/}
+    vt=$(printf 'vt%02d' "${TTY#tty}")
+
+    if [ -z ${xinitrc} ]; then
+        xinitrc="$HOME/.xinitrc"
+    fi
+
+    echo "starting X with parameters: $@"  | ts
+    startx "$xinitrc" "${@}" -- "$vt" 2>&1 | ts | tee /tmp/x_out
+    # bug in X: redirecting stderr crashes it ^^^
+
+    clear
+    echo "X has exited. Output has been saved to /tmp/x_out." | ts
+    echo "Press enter to relog."
+    read enter
+    exit
+}
+
+function xinteractive {
+    # display a menu if no argument is given, otherwise assume choice from argument
+    echo -en "\n"
+
+    if [[ -z "$1" ]]; then
+        echo -n "Enter choice ([i] i3, [f] fluxbox, [r] restart, [h] halt, [c] console): "
+        read c
+    else
+        c="$1"
+    fi
+
+    echo -en "\n"
+
+    case $c[1] in
+        i) start_x i3 ;;
+        f) start_x fluxbox ;;
+
+        r) systemctl reboot ;;
+        h) systemctl poweroff ;;
+        c) return ;;
+
+        *) exit ;;
+    esac
+
+}
+
+# start X if we're on tty1
+function xauto {
+    if [[ -n ${1} ]]; then
+        export xinitrc=${1}
+    fi
+    if [[ -z $DISPLAY ]] && [[ $(tty) =~ "/dev/tty[1-9]" ]]; then
+        if [[ $(tty) = "/dev/tty1" ]] && [[ ! -f ~/x_noauto ]]; then
+            xinteractive i
+        else
+            xinteractive
+        fi
+    fi
+}
