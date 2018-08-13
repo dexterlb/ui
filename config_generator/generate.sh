a9b2019b1dc1f7f41be46d67f9d82f88bea841c9
Angel Angelov <angel.angelov@chaosgroup.com>
2018-08-13 18:33:59 +0300

    make i3bar pretty and transparent

--- config_generator/generate.sh
+++ config_generator/generate.sh
@@ -12,7 +12,7 @@ fi
 
 function get_vars {
     set | sed 's/=.*//g' | while read name; do
-        echo -n "${name}=${(P)name//\0/}"
+        echo -n "${name}=${(P)name//\\0/}"
         echo -en '\0'
     done
 }
@@ -31,7 +31,9 @@ function find_vars_and_gen_config {
             source ${vars_script}
         done
 
-        for vars_script in "${2}".*.vars.sh; do
+        vars_scripts=("${2}".*.vars.sh(N))
+
+        for vars_script in ${vars_scripts[@]}; do
             (
                 echo "using ${vars_script}"
                 source "${vars_script}"
@@ -39,7 +41,7 @@ function find_vars_and_gen_config {
             )
         done
 
-        if [[ -z "${2}".*.vars.sh ]]; then
+        if [[ ${vars_scripts[#]} -eq 0 ]]; then
             gen_config "${1}" "${2}"
         fi
     )
--- ui/screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-xrandr | perl -ne 'print "$1\n" if m/primary ([\d]+x[\d]+)/;' | head -n1 \
-       | read screen_size
-if [[ -z "${screen_size}" ]]; then
-    xrandr | perl -ne 'print "$1\n" if m/connected ([\d]+x[\d]+)/;' | head -n1 \
-           | read screen_size
-fi
-echo "${screen_size}" | read -d'x' screen_width screen_height
