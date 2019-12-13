03da6783d4cb321c237ec1f0a209ca6d57c5df59
dexterlb <dexterlb@qtrp.org>
2019-12-13 12:45:45 +0200

    remote wayland support

--- ui/i3/config.i3.vars.sh
+++ ui/i3/config.i3.vars.sh
@@ -1,2 +1,3 @@
 i3_version=i3_gaps
 screenshooter="${ui_dir}"/screenshot_xorg.sh
+session_mode=local
--- ui/i3/config.sway_remote.vars.sh
+++ ui/i3/config.sway_remote.vars.sh
@@ -1,3 +1,3 @@
 i3_version=sway
 screenshooter="${ui_dir}"/screenshot_wayland.sh
-session_mode=local
+session_mode=remote
