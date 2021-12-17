cd3268e7035a62189d54fcaa4b46f599ea3aac2c
dexterlb <dexterlb@qtrp.org>
2021-12-18 00:38:26 +0200

    support "no batteries"

--- vars.sh
+++ vars.sh
@@ -1,7 +1,14 @@
 #!/bin/zsh
 
+setopt null_glob
+
 ui_dir="$(dirname "${0}")"/ui
 i3_dir="${ui_dir}"/i3
 hostname="$(hostname)"
+
+num_batteries=0
+for f in /sys/class/power_supply/BAT*; do
+    num_batteries=$((num_batteries + 1))
+done
 
 . "${ui_dir}"/visual.sh
