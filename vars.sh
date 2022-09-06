1ed6410674544d5b19b94f01268bcbcbb260174d
dexterlb <dexterlb@qtrp.org>
2022-09-06 18:48:20 +0300

    use secrets.sh

--- vars.sh
+++ vars.sh
@@ -2,7 +2,9 @@
 
 setopt null_glob
 
-ui_dir="$(dirname "${0}")"/ui
+cdir="$(dirname "$(readlink -f "${0}")")"
+
+ui_dir="${cdir}"/ui
 i3_dir="${ui_dir}"/i3
 hostname="$(hostname)"
 
@@ -10,5 +12,7 @@ num_batteries=0
 for f in /sys/class/power_supply/BAT*; do
     num_batteries=$((num_batteries + 1))
 done
+
+. "${cdir}"/secrets.sh
 
 . "${ui_dir}"/visual.sh
